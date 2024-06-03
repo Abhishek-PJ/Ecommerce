ef6613509c32921fbb4817ed171362eb30a4378d
Abhishek Nalatawad <134145130+Abhishek-PJ@users.noreply.github.com>
2024-06-03 23:52:13 +0530

    Changed firebase configurations

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -1,7 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 
 // const firebaseConfig = {
 //     apiKey: "AIzaSyDBHTUtOeuCON7AEF3UPPxU1sHsnFfBv-M",
@@ -12,18 +8,27 @@ import { getAuth } from 'firebase/auth';
 //     appId: "1:609664686595:web:302bf25cc8efa64a4e4c36"
 // };
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD4E72VkIynXf-uqLUqPc4yywGCH1IRGuQ",
-  authDomain: "e-commerce-4e67e.firebaseapp.com",
-  projectId: "e-commerce-4e67e",
-  storageBucket: "e-commerce-4e67e.appspot.com",
-  messagingSenderId: "515303367724",
-  appId: "1:515303367724:web:eacba6a647ea4467999d89",
-  measurementId: "G-E32MFZZCND"
+  apiKey: "AIzaSyD7xxni01wZOvfrkbd2Q5kJI2CIOrluFdc",
+  authDomain: "ecommerce-react-e6aef.firebaseapp.com",
+  projectId: "ecommerce-react-e6aef",
+  storageBucket: "ecommerce-react-e6aef.appspot.com",
+  messagingSenderId: "861948693140",
+  appId: "1:861948693140:web:54e16e5f26fd42fb68c281",
+  measurementId: "G-4NKKDYW419"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
 
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
