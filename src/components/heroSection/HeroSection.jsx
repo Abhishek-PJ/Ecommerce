e36785735f3a216e0bc0acf9fae03ccbef6824c1
Abhishek Nalatawad <134145130+Abhishek-PJ@users.noreply.github.com>
2024-06-05 14:59:41 +0530

    Added more herosections

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -8,21 +8,29 @@ function HeroSection() {
         <Carousel.Item>
           <img
             className="d-block w-full rounded-md"
-            src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2024/5/21/91b180cd-de53-4a07-8cd2-3e5b8d37efd41716229933950-Desktop-Banner.jpg"
+            src="https://assets.ajio.com/cms/AJIO/WEB/D-1.0-MHP-30052024-MainBanner-Z1-P1-5090.gif"
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-full rounded-md"
-            src="https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/b656a7f4-4688-4997-bb7c-54b78793981e1658752386588-Western-Wear_Desk.jpg"
+            src="https://assets.ajio.com/cms/AJIO/WEB/D-1.0-WHP-050624-DAILYBANNER-BU-Z5-P4-Aurelia-Fabindia-min40.jpg"
+            
             alt="Second slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-full rounded-md"
-            src="https://4.bp.blogspot.com/-j08zU37hpt4/W5aaDndpsWI/AAAAAAAAFoc/tq-c11-V1sgMDyFd5cB3Z6jsO2UICZiQgCK4BGAYYCw/s1600/CL-Banner.jpg"
+            src="https://assets.ajio.com/cms/AJIO/WEB/D-1.0-MHP-05062024-dailybannerbu-z5-p4-LOUISPHILIPPE-LEECOOPER-30-80.jpg"
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-full rounded-md"
+            src="https://assets.ajio.com/cms/AJIO/WEB/D-1.0-MHP-05062024-topbanner-z2-p1-LEECOOPER-JOHNPLAYER-MIN50.jpg"
             alt="Third slide"
           />
         </Carousel.Item>
--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -10,7 +10,6 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
@@ -29,8 +28,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
 
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
