78be2c3dac87e657c5d6bab2d428edc1cd3e0f53
Abhishek Nalatawad <abhia7535@gmail.com>
2024-07-11 11:53:37 +0530

    Update OrderDetail.jsx
    updated border colour

--- src/components/admin/OrderDetail.jsx
+++ src/components/admin/OrderDetail.jsx
@@ -12,7 +12,7 @@ const OrderDetail = () => {
     return (
         <div>
             <div className="py-5">
-                <h1 className="text-xl text-pink-300 font-bold">All Orders</h1>
+                <h1 className="text-xl text-pink-600 font-bold">All Orders</h1>
             </div>
             <div className="w-full overflow-x-auto">
                 <table className="w-full text-left border border-collapse sm:border-separate border-pink-100 text-pink-400">
@@ -39,23 +39,23 @@ const OrderDetail = () => {
                             order.cartItems.map((item, itemIndex) => {
                                 const { id, productImageUrl, title, category, price, quantity } = item;
                                 return (
-                                    <tr key={`${order.id}-${item.id}`} className="text-pink-300">
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{orderIndex + 1}.{itemIndex + 1}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{id}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500"><img src={productImageUrl} alt="Product" /></td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{title}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{category}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">₹{price}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{quantity}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">₹{price * quantity}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{order.status}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{order.addressInfo.name}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{order.addressInfo.address}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{order.addressInfo.pincode}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{order.addressInfo.mobileNumber}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{order.email}</td>
-                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-slate-500">{order.date}</td>
-                                        <td onClick={() => orderDelete(order.id)} className="h-12 px-6 text-md border-t border-l first:border-l-0 border-pink-100 text-red-500 cursor-pointer">Delete</td>
+                                    <tr key={`${order.id}-${item.id}`} className="text-pink-600">
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{orderIndex + 1}.{itemIndex + 1}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{id}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800"><img src={productImageUrl} alt="Product" /></td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{title}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{category}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">₹{price}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{quantity}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">₹{price * quantity}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{order.status}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{order.addressInfo.name}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{order.addressInfo.address}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{order.addressInfo.pincode}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{order.addressInfo.mobileNumber}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{order.email}</td>
+                                        <td className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-slate-800">{order.date}</td>
+                                        <td onClick={() => orderDelete(order.id)} className="h-12 px-6 text-md border-t border-l first:border-l-0 border-slate-300 text-red-500 cursor-pointer">Delete</td>
                                     </tr>
                                 );
                             })
